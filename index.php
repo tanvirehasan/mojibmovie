57882f247170e01ebeb37cbc01afe0262e22fb2b
TANVIR HASAN <tanvirhasanbcse@gmail.com>
2021-11-24 17:49:20 +0600

    success
    success

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://kit-pro.fontawesome.com/releases/v5.15.2/css/pro.min.css">
     <link rel="stylesheet" href="assets/js/jquery.flipster.min.css">
     <title></title>
   </head>
-  <body>
-
+<body>
 <header>
     <!-- ================ MANUBAR ================== -->
     <nav class="navbar navbar-expand-lg navbar-light">
@@ -85,7 +85,7 @@
             <div class="col-md-5">
                 <h3>সিনোপসিস</h3>
                 <p>
-                    জাতির পিতা বঙ্গবন্ধু শেখ মুজিবুর রহমান এর “অসমাপ্ত আত্মজীবনী” অবলম্বনে ১৯৪৯ থেকে ১৯৫২ পর্যন্ত গণ মানুষের নেতা শেখ মুজিব হয়ে ওঠার প্রেক্ষাপট এবং মহান ভাষা আন্দোলনে তার ভূমিকা নিয়ে নির্মিত চলচ্চিত্র “চিরঞ্জীব মুজিব”
+                    জাতির পিতা বঙ্গবন্ধু শেখ মুজিবুর রহমান এর “অসমাপ্ত আত্মজীবনী” অবলম্বনে ১৯৪৯ থেকে ১৯৫২ পর্যন্ত গণ মানুষের নেতা শেখ মুজিব হয়ে ওঠার প্রেক্ষাপট এবং মহান ভাষা আন্দোলনে তার ভূমিকা নিয়ে নির্মিত চলচ্চিত্র “চিরঞ্জীব মুজিব” শেখ হাসিনা ও শেখ রেহানা নিবেদিত শেখ মুজিবুর রহমানের অসমাপ্ত আত্মজীবনী অবলম্বনে পূর্ণদৈর্ঘ্য চলচ্চিত্র
                 </p>
             </div>
         </div>
@@ -118,10 +118,11 @@
             <span class="m-0">Chironjib Mujib Official Trailer</span>
         </div>
         <div class="row">
-            <div class="col-md-3"><img src="assets/img/slider/text1.gif" alt="Chironjib Mujib Dialogues"></div>
-            <div class="col-md-3"><img src="assets/img/slider/text2.gif" alt="Chironjib Mujib Dialogues"></div>
-            <div class="col-md-3"><img src="assets/img/slider/text3.gif" alt="Chironjib Mujib Dialogues"></div>
-            <div class="col-md-3"><img src="assets/img/slider/text4.gif" alt="Chironjib Mujib Dialogues"></div>
+            <div class="col-md-3"><img src="assets/img/qutation/01.gif" alt="Chironjib Mujib Dialogues"></div>
+            <div class="col-md-3"><img src="assets/img/qutation/02.gif" alt="Chironjib Mujib Dialogues"></div>
+            <div class="col-md-3"><img src="assets/img/qutation/03.gif" alt="Chironjib Mujib Dialogues"></div>
+            <div class="col-md-3"><img src="assets/img/qutation/04.gif" alt="Chironjib Mujib Dialogues"></div>
+
         </div>
     </div>
 </section>
@@ -313,9 +314,43 @@
     </div>
 </section>
 
-
-<footer>
+<!-- ============= FOOTER ================= -->
+<footer class="footer bg-black pt-5">
+    <div class="container">
+        <div class="row">
+            <!-- Footer 1 -->
+            <div class="col-md-4">
+                <img src="assets/img/logo/logored.png" alt="">
+                <p class="footer-text">শেখ হাসিনা ও শেখ রেহানা নিবেদিত শেখ মুজিবুর রহমানের অসমাপ্ত আত্মজীবনী অবলম্বনে পূর্ণদৈর্ঘ্য চলচ্চিত্র</p>
+            </div>
+            <!-- Footer 2 -->
+            <div class="col-md-4">                    
+                <ul>
+                    <h5 class="h6 text-white mb-3"><i class="fas fa-map-marked-alt"></i> Useful Links</h5>
+                    <li><a href="">Buy Ticket</a></li>
+                    <li><a href="">About Moive</a></li>
+                    <li><a href="">Terms and Conditions</a></li>
+                    <li><a href="">Coupons</a></li>
+                    <li><a href="">Privacy Policy</a></li>
+                    <li><a href="">Team</a></li>
+                    <li><a href="">Gallery</a></li>
+                </ul>                
+            </div>
+            <!-- Footer 3 -->
+            <div class="col-md-4">
+                <p class="text-white fw-bold"><i class="fad fa-globe"></i> Contact Us</p>
+                <address class="text-secondary">চিরঞ্জীব মুজিব <br>Phone No. 01843640517 <br> Address: House No-53, First Floor, Road No-1, A Block, Niketan, Gulsan-1.</address>
+                <button class="btn btn-lg bg-primary text-white me-2"><i class="fab fa-facebook-f"></i></button>
+                <button class="btn btn-lg  text-white me-2" style="background: #FE2E64;"><i class="fab fa-instagram"></i></button>
+                <button class="btn btn-lg  bg-info text-white me-2 "><i class="fab fa-linkedin-in"></i></button>
+                <button class="btn btn-lg  bg-danger text-white "><i class="fab fa-youtube"></i></button>
+            </div>
+        </div>
+    </div> 
     
+    <div class="sub_footer text-center py-3">
+        <p>Copyright &copy; চিরঞ্জীব মুজিব - <?php echo date('Y');?> | Developed By <a href="//tigerdenit.com">TigerDen IT</a></p>
+    </div>
 </footer>
 
 
